d652eb66e662ab16f6b69130d9f4d9752c405b95
lucassantos0 <lubs123@gmail.com>
2017-11-13 15:19:10 -0200

    nav back fix

--- controller/MapaComparativoDetalhes.controller.js
+++ controller/MapaComparativoDetalhes.controller.js
@@ -1,13 +1,21 @@
-sap.ui.define(
-[
-	"lrs/ui5/controller/BaseController"
-], function(BaseController)
-{
+sap.ui.define([ "lrs/ui5/controller/BaseController",
+		"sap/ui/core/routing/History" ], function(BaseController, History) {
 	"use strict";
 	return BaseController.extend("lrs.ui5.controller.MapaComparativoDetalhes",
-	{
-		onInit : function()
-		{
-		}
-	});
+			{
+				onInit : function() {
+				},
+				onNavBack : function() {
+					var oHistory = History.getInstance();
+					var sPreviousHash = oHistory.getPreviousHash();
+
+					if (sPreviousHash !== undefined) {
+						window.history.go(-1);
+					} else {
+						var oRouter = sap.ui.core.UIComponent
+								.getRouterFor(this);
+						oRouter.navTo("home", {}, true);
+					}
+				}
+			});
 });
