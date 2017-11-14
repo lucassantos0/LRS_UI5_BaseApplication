04935dd6185d650bcc2b8b7cad33bfda64008b93
lucassantos0 <lubs123@gmail.com>
2017-11-14 11:24:35 -0200

    fix-formatter

--- controller/ConsumoContratos.controller.js
+++ controller/ConsumoContratos.controller.js
@@ -1,16 +1,14 @@
-sap.ui.define(
-[
-	"lrs/ui5/controller/BaseController"
-], function(BaseController)
-{
-	"use strict";
-	return BaseController.extend("lrs.ui5.controller.ConsumoContratos",
-	{
-		onInit : function()
-		{
-		}
-		formatterMasterListDate : function(sDate) {
-			return sDate.substring(6,8) + "/" + sDate.substring(4,6) + "/" + substring(1,4);
-		}
-	});
-});
+sap.ui.define([ "lrs/ui5/controller/BaseController" ],
+		function(BaseController) {
+			"use strict";
+			return BaseController.extend("lrs.ui5.controller.ConsumoContratos",
+					{
+						onInit : function() {
+						},
+						formatterMasterListDate : function(sDate) {
+							return sDate.substring(6, 8) + "/"
+									+ sDate.substring(4, 6) + "/"
+									+ substring(1, 4);
+						}
+					});
+		});
