bfc32a4b7d3ac6888ec597f32842346b5e019d48
lucassantos0 <lubs123@gmail.com>
2017-11-14 11:34:49 -0200

    fix-consumo contratos

--- controller/ConsumoContratos.controller.js
+++ controller/ConsumoContratos.controller.js
@@ -10,6 +10,12 @@ sap.ui.define([ "lrs/ui5/controller/BaseController" ],
 							return str.substring(6, 8) + "/"
 									+ str.substring(4, 6) + "/"
 									+ str.substring(0, 4);
+						},
+						formatterMasterListConsumoStatus : function(sConsumo) {
+							var iPorcentagem = parseInt(sConsumo);
+							if(iPorcentagem < 40) { return 'None'; }
+							if(iPorcentagem < 80) { return 'Warning'; }
+							return 'Error';
 						}
 					});
 		});
