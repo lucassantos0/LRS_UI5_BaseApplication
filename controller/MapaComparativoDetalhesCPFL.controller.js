94f182379ca30e8068f4eba06cd25f8e8676a2c8
lucassantos0 <lubs123@gmail.com>
2018-01-16 12:27:01 -0200

    fix mapa routing target

--- controller/MapaComparativoDetalhesCPFL.controller.js
+++ controller/MapaComparativoDetalhesCPFL.controller.js
@@ -6,17 +6,17 @@ sap.ui.define([ "lrs/ui5/controller/BaseController" ], function(BaseController,
 				onInit : function() {
 				},
 				onRouteMatched : function(oEvent) {
-					var sLifnr = oEvent.getParameters().arguments.rfq;
-					if (!rfq)
+					var sRfq = oEvent.getParameters().arguments.rfq;
+					if (!sRfq)
 						return false;
 					
 					this.byId("mapaComparativoDetalhesCPFL").setBindingContext();
-					var oBinding = this.byId("tableTodosTrechos").getBinding(
+				/*	var oBinding = this.byId("tableTodosTrechos").getBinding(
 							"rows");
 					var aFilters = [];
 					aFilters.push(new sap.ui.model.Filter("Lifnr",
 							sap.ui.model.FilterOperator.EQ, sLifnr));
-					oBinding.filter(aFilters);
+					oBinding.filter(aFilters);/*
 				}
 			});
 });
--- controller/MapaComparativoLista.controller.js
+++ controller/MapaComparativoLista.controller.js
@@ -10,8 +10,8 @@ sap.ui.define(
 		{
 		},
 		onListItemPress : function(oEvent) {
-			var sId = this.getModel("listaRfqs").getObject(oEvent.getSource().getBindingContextPath()).id;
-			this.getRouter().navTo("MapaComparativoDetalhesCPFL",{ rfq : sId });
+			//var sId = this.getModel("listaRfqs").getObject().id;
+			this.getRouter().navTo("MapaComparativoDetalhesCPFL",{ rfq : oEvent.getSource().getBindingContextPath() });
 		}
 	});
 });
