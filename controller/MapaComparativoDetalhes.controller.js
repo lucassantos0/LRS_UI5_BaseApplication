dc52822feeba8953c921bd0dabd430b37e5f276a
lucassantos0 <lubs123@gmail.com>
2018-01-16 11:36:16 -0200

    fix oxygen 2 release - test commit

--- controller/MapaComparativoDetalhes.controller.js
+++ controller/MapaComparativoDetalhes.controller.js
@@ -1,9 +1,10 @@
-sap.ui.define([ "lrs/ui5/controller/BaseController" ], function(BaseController, History) {
+sap.ui.define([ "lrs/ui5/controller/BaseController" ], function(BaseController,
+		History) {
 	"use strict";
 	return BaseController.extend("lrs.ui5.controller.MapaComparativoDetalhes",
 			{
 				onInit : function() {
 				}
-				
+
 			});
 });
