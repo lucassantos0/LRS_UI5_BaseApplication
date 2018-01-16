5ae8ba35efcbb146f6151ffeb7ca907cc0aaf1a7
lucassantos0 <lubs123@gmail.com>
2018-01-16 13:34:31 -0200

    binding detalhes mapa

--- controller/MapaComparativoDetalhesCPFL.controller.js
+++ controller/MapaComparativoDetalhesCPFL.controller.js
@@ -1,16 +1,21 @@
 sap.ui.define([ "lrs/ui5/controller/BaseController" ], function(BaseController,
 		History) {
 	"use strict";
-	return BaseController.extend(
-			"lrs.ui5.controller.MapaComparativoDetalhesCPFL", {
-				onInit : function() {
-				},
-				onRouteMatched : function(oEvent) {
-					var sRfq = decodeURIComponent(oEvent.getParameters().arguments.rfq);
-					if (!sRfq)
-						return false;
-					
-					//this.byId("mapaComparativoDetalhesCPFL").setBindingContext();
-				}
-			});
+	return BaseController
+			.extend("lrs.ui5.controller.MapaComparativoDetalhesCPFL",
+					{
+						onInit : function() {
+						},
+						onRouteMatched : function(oEvent) {
+							var sPath = decodeURIComponent(oEvent
+									.getParameters().arguments.rfq);
+							if (!sPath) {
+								return false;
+							}
+							var oModel = this.getModel("eventosSourcing")
+									.getObject(sPath);
+							this.byId("mapaComparativoDetalhesCPFL")
+									.bindContext("eventosSourcing>" + sPath);
+						}
+					});
 });
