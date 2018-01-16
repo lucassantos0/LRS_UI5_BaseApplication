11a9251afdcc4f1357c224bc05eaf42429e2c26e
lucassantos0 <lubs123@gmail.com>
2018-01-16 13:35:59 -0200

    binding

--- controller/MapaComparativoDetalhesCPFL.controller.js
+++ controller/MapaComparativoDetalhesCPFL.controller.js
@@ -5,6 +5,7 @@ sap.ui.define([ "lrs/ui5/controller/BaseController" ], function(BaseController,
 			.extend("lrs.ui5.controller.MapaComparativoDetalhesCPFL",
 					{
 						onInit : function() {
+							this.getRouter().getRoute("MapaComparativoDetalhesCPFL").attachMatched(this.onRouteMatched, this);
 						},
 						onRouteMatched : function(oEvent) {
 							var sPath = decodeURIComponent(oEvent
