47072cc9042da937c044dd9217cd8ebacb5f8d59
wodtke <marcel@wodtke.com.br>
2018-06-21 14:26:28 -0300

    previas

--- controller/Previas.controller.js
+++ controller/Previas.controller.js
@@ -8,5 +8,22 @@ sap.ui.define(
 	{
 		onInit : function(){
 		},
+		formatterMasterListDate : function(sDate) {
+			var str = sDate;
+			return str.substring(6, 8) + "/"
+					+ str.substring(4, 6) + "/"
+					+ str.substring(0, 4);
+		},
+		formatterMasterListConsumoStatus : function(sConsumo) {
+			var iPorcentagem = parseInt(sConsumo);
+			if(iPorcentagem < 40) { return 'None'; }
+			if(iPorcentagem < 80) { return 'Warning'; }
+			return 'Error';
+		},
+		onSelecionarContrato : function(oEvent){
+			var sPath = oEvent.getSource().getBindingContextPath();
+			var oModel = this.getModel("consumoContratos").getObject(sPath);
+			this.byId("detailPageContratos").bindContext("consumoContratos>" + sPath);
+		}		
 	});
 });
