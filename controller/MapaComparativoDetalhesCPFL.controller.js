8c2e5468b75eee15172d5babd2f734c643ffcb42
Wodtke <Wodtke@192.168.50.3>
2019-04-01 14:40:48 -0300

    mapa

--- controller/MapaComparativoDetalhesCPFL.controller.js
+++ controller/MapaComparativoDetalhesCPFL.controller.js
@@ -24,47 +24,46 @@ sap.ui.define([ "lrs/ui5/controller/BaseController", 'sap/ui/model/json/JSONMode
 					this._bindingPath = "eventosSourcing>/eventosSourcing/" + nEntry;
 					this.byId("mapaComparativoDetalhesCPFL").bindContext(this._bindingPath);
 					
-					debugger;
-//		            var oModelTable = {
-//		                    ColumnsId : this.ItemColumns.oData.columns,
-//		                    Data : this.ItemColumns.oData.rows
-//		                };
-//		            
-//					var oTable = this.byId("tableItems");  
-//					var oModel = new sap.ui.model.json.JSONModel(oModelTable);
-//					oTable.setModel(oModel);
-//					oTable.bindAggregation("columns", "/ColumnsId", new sap.m.Column({
-//			            hAlign : "{halign}",
-//			            width : "{width}",
-//			            visible: "{visible}",
-//			            header : new sap.m.Text({
-//			                text : "{text}"
-//			            }),
-//			            styleClass:"{style}"
-//			        }));
-//	   			    
-//				    oTable.bindItems("/Data", function(index, context) {
-//				        var obj = context.getObject();
-//				        var row = new sap.m.ColumnListItem();
-//				        for(var k in obj) {	
-//				        	var diver = "div" + k; 
-//				        	if(obj[diver] == "X"){
-//				        		
-//				        		row.addCell(new sap.m.HBox({
-//				        						width:"100%",
-//				        						justifyContent : sap.m.FlexJustifyContent.SpaceAround,
-//				        						items: [    new sap.m.Text({text : obj[k]}),
-//				        							new sap.ui.core.Icon({ src : "sap-icon://color-fill",
-//				        												   color : "#f33334"})
-//				        							  ]
-//				            }));	
-//				            
-//				        	}else{
-//				        		row.addCell(new sap.m.Text({text : obj[k]}));
-//				        	}
-//				        }
-//				        return row;
-//				    });	
+		            var oModelTable = {
+		                    ColumnsId : this.getView().getModel("mapaLadoaLado").oData.columns,
+		                    Data : this.getView().getModel("mapaLadoaLado").oData.rows
+		                };
+		            
+					var oTable = this.byId("tableLadoaLado");  
+					var oModel = new sap.ui.model.json.JSONModel(oModelTable);
+					oTable.setModel(oModel);
+					oTable.bindAggregation("columns", "/ColumnsId", new sap.m.Column({
+			            hAlign : "{halign}",
+			            width : "{width}",
+			            visible: "{visible}",
+			            header : new sap.m.Text({
+			                text : "{text}"
+			            }),
+			            styleClass:"{style}"
+			        }));
+	   			    
+				    oTable.bindItems("/Data", function(index, context) {
+				        var obj = context.getObject();
+				        var row = new sap.m.ColumnListItem();
+				        for(var k in obj) {	
+				        	var diver = "div" + k; 
+				        	if(obj[diver] == "X"){
+				        		
+				        		row.addCell(new sap.m.HBox({
+				        						width:"100%",
+				        						justifyContent : sap.m.FlexJustifyContent.SpaceAround,
+				        						items: [    new sap.m.Text({text : obj[k]}),
+				        							new sap.ui.core.Icon({ src : "sap-icon://color-fill",
+				        												   color : "#f33334"})
+				        							  ]
+				            }));	
+				            
+				        	}else{
+				        		row.addCell(new sap.m.Text({text : obj[k]}));
+				        	}
+				        }
+				        return row;
+				    });	
 				    
 				}		
 			
