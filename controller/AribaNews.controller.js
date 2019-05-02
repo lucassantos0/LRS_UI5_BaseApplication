4e290f094ef76e11a14f28453977ee62df09ff44
lucassantos0 <lubs123@gmail.com>
2019-05-02 15:53:00 -0300

    AribaNews controller

---
+++ controller/AribaNews.controller.js
@@ -0,0 +1,20 @@
+sap.ui.define(
+[
+		"lrs/ui5/controller/BaseController", "jquery.sap.global", "sap/ui/core/Fragment",
+		"sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel", "sap/m/ResponsivePopover", "sap/m/MessagePopover",
+		"sap/m/ActionSheet", "sap/m/Button", "sap/m/Link", "sap/m/Bar", "sap/ui/layout/VerticalLayout",
+		"sap/m/NotificationListItem", "sap/m/MessagePopoverItem", "sap/ui/core/CustomData", "sap/m/MessageToast"
+], function(BaseController, jQuery, Fragment, Controller, JSONModel, ResponsivePopover, MessagePopover, ActionSheet,
+		Button, Link, Bar, VerticalLayout, NotificationListItem, MessagePopoverItem, CustomData, MessageToast)
+{
+	"use strict";
+
+	return BaseController.extend("lrs.ui5.controller.AribaNews",
+	{
+
+		onInit : function()
+		{
+		}
+
+	});
+});
